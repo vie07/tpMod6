7bc918b235c44fceb968c080384c7cbcb37e07e1
Ravie <aryaravie8@gmail.com>
2026-04-06 04:38:29 +0700

    menambahkan class SayaMusicTrack

--- Program.cs
+++ Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class SayaMusicTrack
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaMusicTrack(string title)
+    {
+        Random rnd = new Random();
+        this.id = rnd.Next(10000, 99999); //generate id
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count; //nambah
+    }
+
+    //print semua
+    public void PrintTrackDetails()
+    {
+        Console.WriteLine("ID: " + id);
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Play Count: " + playCount);
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaMusicTrack lagu1 = new SayaMusicTrack("Hindia - Evaluasi");
+
+        lagu1.IncreasePlayCount(5000); // test
+
+        lagu1.PrintTrackDetails();
+    }
+}
--- obj/Debug/net10.0/TP_MODUL6_103022400147.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_MODUL6_103022400147.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_103022400147")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184cb399911f7a5a77a0827ddf5d2abf619064d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_103022400147")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_103022400147")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
